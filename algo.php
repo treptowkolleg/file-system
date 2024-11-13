367d9cc0a380e94de56d7ec7c6fbe3fc58fc7184
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-11-13 07:33:21 +0100

    update

---
+++ algo.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Lösungsmenge zur Ermittelung der Tage, wann sich beiden Monde gegenseitig beschatten:" . PHP_EOL;
+
+for ($k = 0; $k < 10; $k++) {
+    $d = 5 - 5/6 + 10 * $k;
+    echo "bei $d Tagen" . PHP_EOL;
+}
+
